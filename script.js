cf0f6d8e29566f232ed254166641c89a245fd34b
EdmilsonOffice <160644536+EdmilsonOffice@users.noreply.github.com>
2024-04-19 17:19:05 +0000

    docs: Projeto Crud

---
+++ script.js
@@ -0,0 +1,12 @@
+const openModal = () => {
+    document.getElementById('modal').classList.add('active')
+}
+
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active')
+
+}
+
+document.getElementById('cadastrarUsuario').addEventListener('click', openModal);
+
+document.getElementById('modal-close').addEventListener('click', closeModal);
